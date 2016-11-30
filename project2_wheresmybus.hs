a6d3243c3d4653dca2a325acd9a74e5c58181d13
Li Jye Tong <ljtong1@gmail.com>
2016-11-30 13:52:40 -0800

    Added in example query for Translink API

--- project2_wheresmybus.hs
+++ project2_wheresmybus.hs
@@ -1,3 +1,5 @@
 import Network.HTTP
 
 key = "cURsrZ7AjbHgfC8cuZLV"
+
+getStop = simpleHTTP (getRequest ("http://api.translink.ca/rttiapi/v1/stops/52026/estimates?apikey=" ++ key)) >>= fmap (take 10000) . getResponseBody
