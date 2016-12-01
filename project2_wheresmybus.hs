9d0c0765435dac331e9fce0097b703b287f0aad7
Li Jye Tong <ljtong1@gmail.com>
2016-11-30 22:40:58 -0800

    API access working

--- project2_wheresmybus.hs
+++ project2_wheresmybus.hs
@@ -1,5 +1,14 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 import Network.HTTP
+import Network.HTTP.Headers
+import Network.URI
+import qualified Data.ByteString.Lazy.Char8 as C
+import Text.XML.Light
 
-key = "cURsrZ7AjbHgfC8cuZLV"
+key' = "cURsrZ7AjbHgfC8cuZLV"
+url = "http://api.translink.ca/rttiapi/v1/stops/52026/estimates?apikey=" ++ key'
+Just uri = parseURI url
+header' = Header HdrAccept "application/JSON"
 
-getStop = simpleHTTP (getRequest ("http://api.translink.ca/rttiapi/v1/stops/52026/estimates?apikey=" ++ key)) >>= fmap (take 10000) . getResponseBody
+request = simpleHTTP (Request uri GET [header'] "") >>= fmap (Prelude.take 10000) . getResponseBody
