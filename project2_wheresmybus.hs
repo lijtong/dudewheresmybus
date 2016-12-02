a76f781acf3141f55bbfed147c193b14bc38ac04
Li Jye Tong <ljtong1@gmail.com>
2016-12-02 12:48:32 -0800

    No longer need to read from file to read JSON data, can read it directly from the string

--- project2_wheresmybus.hs
+++ project2_wheresmybus.hs
@@ -20,13 +20,18 @@ import Network.URI
 import qualified Data.ByteString.Lazy.Char8 as C
 import Text.XML.Light
 import Data.Char
+import Control.Concurrent
 
+testurl = "http://maps.googleapis.com/maps/api/geocode/json?address=2366+Main+Mall,+Vancouver,+BC"
 key' = "cURsrZ7AjbHgfC8cuZLV"
-url = "http://api.translink.ca/rttiapi/v1/stops/52026/estimates?apikey=" ++ key'
-Just uri = parseURI url
+geocachekey = "AIzaSyAhSWqsa_BRpEJtM5FGhSa3ZGJjQ0FWByg"
+url = "http://api.translink.ca/rttiapi/v1/stops/"
+--url = "http://api.translink.ca/rttiapi/v1/stops/51947/estimates?apikey=" ++ key'
+estimates = "estimates?apikey=" ++ key'
+--Just uri = parseURI url
 header' = Header HdrAccept "application/JSON"
 
-request = simpleHTTP (Request uri GET [header'] "") >>= fmap (Prelude.take 10000) . getResponseBody
+--request = simpleHTTP (Request uri GET [header'] "") >>= fmap (Prelude.take 10000) . getResponseBody
 
 -- !!! COPIED
 
@@ -44,48 +49,63 @@ request = simpleHTTP (Request uri GET [header'] "") >>= fmap (Prelude.take 10000
 -- !!!
 main = do
   putStrLn "Hello! Welcome to where's my bus! We'll get you your bus schedule."
-  test <- simpleHTTP (Request uri GET [header'] "") >>= fmap (Prelude.take 10000) . getResponseBody
+  main1
+
+main1 = do
+  putStrLn "Input your bus stop number now"
+  bstop <- getLine
+  if (not (isValidBus bstop)) then do 
+		putStrLn "Invalid Bus #, try again!"
+		main1
+  else do
+	let queryurl = url ++ bstop ++ "/" ++ estimates
+	let Just uri = parseURI queryurl	
+	print uri
+	test <- simpleHTTP (Request uri GET [header'] "") >>= fmap (Prelude.take 10000) . getResponseBody
   -- !!!! CURRENTLY, test acts as a string.
   -- putStrLn test
-  -- let jsonData = read test :: JSON
+	-- let jsonData = read test :: JSON
+	-- print $ jsonData
+	-- writeFile "data'.json" test
+	-- threadDelay 5000000
   -- let jone = [json| $ show jsonData |]
   --     val1 = [jsq| jone[1].Direction |] :: String
   -- putStrLn $ show val1
       -- -- putStrLn $ show jsonData
       -- print $ (jsonData ? "[0]" :: JSON)
-
-  let jone = [jsonF|data.json|]
-      val1 = [jsq| jone[0].Direction |] :: String
-      val2 = [jsq| jone[0].Schedules[0].Destination |]     :: String
-	  -- val3 = [jsq| jone[1].ExpectedLeaveTime |] :: String
-	  -- val4 = [jsq| jone[2].Destination |] :: String
-	  -- val5 = [jsq| jone[2].ExpectedLeaveTime |] :: String
-	  -- val6 = [jsq| jone[3].Destination |] :: String
-	  -- val7 = [jsq| jone[4].ExpectedLeaveTime |] :: String
-  putStrLn val1
-  putStrLn val2
-  -- putStrLn val3
-  -- putStrLn val4
-  -- putStrLn val5
-  -- putStrLn val6
-  -- putStrLn val7
-  main1
+--	let jone = [jsonF|data'.json|]
+	-- Able to read without writing to file now
+	let jone = read test :: JSON
+	let val1 = [jsq| jone[0].Direction |] :: String
+	let val2 = [jsq| jone[0].Schedules[0].Destination |]     :: String
+	let val3 = [jsq| jone[0].Schedules[0].ExpectedLeaveTime |]     :: String
+	let val4 = [jsq| jone[0].Schedules[1].Destination |]     :: String
+	let val5 = [jsq| jone[0].Schedules[1].ExpectedLeaveTime |]     :: String
+	let val6 = [jsq| jone[0].Schedules[2].Destination |]     :: String
+	let val7 = [jsq| jone[0].Schedules[2].ExpectedLeaveTime |]     :: String
+	putStrLn val1
+	putStrLn val2
+	putStrLn val3
+	putStrLn val4
+	putStrLn val5
+	putStrLn val6
+	putStrLn val7
 
 -- prompt user for valid input. Move to step 2 if bus is valid! Otherwise, loop.
-main1 = do
-  bus <- getLine
+-- main1 = do
+ -- bus <- getLine  
+  -- if (isValidBus bus) then
 
-  if (isValidBus bus) then
-    main2
-  else
-    do
-      putStrLn "Invalid Bus #, try again!"
-      main1
+  -- main2
+  -- else
+    -- do
+      -- putStrLn "Invalid Bus #, try again!"
+      -- main1
 
--- action 2: API work goes here
-main2 = do
-  -- something
-  putStrLn "Finished" -- !!!
+-- -- action 2: API work goes here
+-- main2 = do
+  -- -- something
+  -- putStrLn "Finished" -- !!!
 
 ------------------------------
 -- Helper functions belong below, so as to make our "Actions" more clear and simple.
