543c6c11fa582cd33229d8e19d22333910885092
Li Jye Tong <ljtong1@gmail.com>
2016-12-02 21:40:16 -0800

    Brute force method of getting stops nearby, only return 10 max

--- project2_wheresmybus.hs
+++ project2_wheresmybus.hs
@@ -15,24 +15,20 @@ import Data.Ratio
 import Network.HTTP
 import Network.HTTP.Headers
 import Network.URI
---import qualified Data.ByteString.Lazy.Char8 as C
---import Text.XML.Light
 import Data.Char
---import Control.Concurrent
 import Control.Monad
 import System.Exit
 import Data.String
 import qualified Data.Text as T
---import Data.Typeable
 
 testurl = "http://maps.googleapis.com/maps/api/geocode/json?address=2366+Main+Mall,+Vancouver,+BC"
 key' = "cURsrZ7AjbHgfC8cuZLV"
--- geocachekey = "AIzaSyAhSWqsa_BRpEJtM5FGhSa3ZGJjQ0FWByg"
 url = "http://api.translink.ca/rttiapi/v1/stops"
 geo = "http://maps.googleapis.com/maps/api/geocode/json?address="
 estimates = "estimates?apikey=" ++ key'
 routeno = "&routeno="
 header' = Header HdrAccept "application/JSON"
+n = -1
 
 -- preface: program runs in terms of ACTIONS and do blocks.
 --            as one action is finished, you link it to another action.
@@ -99,6 +95,8 @@ main1 = do
 main2 = do
 	putStrLn "Enter the address you would like to know nearby bus stops for"
 	addr <- getLine
+	putStrLn "Enter the radius that you would like to search in metres"
+	rad <- getLine
 	let formataddr = replaceBlank addr
 	let geourl = geo ++ formataddr
 	let Just geouri = parseURI geourl
@@ -118,17 +116,11 @@ main2 = do
 	let long = [jsq| geojson[0].results[0].geometry.location.lng |] :: Double
 	let proplat = (truncate' lat 6)
 	let proplong = (truncate' long 6)
-	let queryurl = url ++ "?" ++ "apikey=" ++ key' ++ "&lat=" ++ (show proplat) ++ "&long=" ++ (show proplong) ++ "&radius=500"
+	let queryurl = url ++ "?" ++ "apikey=" ++ key' ++ "&lat=" ++ (show proplat) ++ "&long=" ++ (show proplong) ++ "&radius=" ++ rad
 	let Just uri = parseURI queryurl	
 	--print uri
 	estquery <- simpleHTTP (Request uri GET [header'] "") >>= fmap (Prelude.take 10000) . getResponseBody
-	let checkquery = ('[':estquery)
-	let checkquery1 = checkquery ++ "]"
-	let estcheck = read checkquery1 :: JSON
-	let code = [jsq| estcheck[0].Code |] :: String
-	let msg = [jsq| estcheck[0].Message |] :: String
-	let estjson = read estquery :: JSON
-	print estjson
+	printstops estquery
 ------------------------------
 -- Helper functions belong below, so as to make our "Actions" more clear and simple.
 
@@ -154,3 +146,93 @@ replaceBlank = map (\c -> if c==' ' then '+'; else c)
 truncate' :: Double -> Int -> Double
 truncate' x n = (fromIntegral (floor (x * t))) / t
     where t = 10^n
+
+printstops estquery = do 
+	let checkquery = ('[':estquery)
+	let checkquery1 = checkquery ++ "]"
+	let estcheck = read checkquery1 :: JSON
+	let code = [jsq| estcheck[0].Code |] :: String
+	let msg = [jsq| estcheck[0].Message |] :: String
+	let estjson = read estquery :: JSON
+	if (code == "null") then do
+		let name1 = [jsq| estjson[0].Name |] :: String
+		let routes1 = [jsq| estjson[0].Routes |]     :: String
+		let stop1 = [jsq| estjson[0].StopNo |]     :: Integer
+		let name2 = [jsq| estjson[1].Name |] :: String
+		let routes2 = [jsq| estjson[1].Routes |]     :: String
+		let stop2 = [jsq| estjson[1].StopNo |]     :: Integer
+		let name3 = [jsq| estjson[2].Name |] :: String
+		let routes3 = [jsq| estjson[2].Routes |]     :: String
+		let stop3 = [jsq| estjson[2].StopNo |]     :: Integer
+		let name4 = [jsq| estjson[3].Name |] :: String
+		let routes4 = [jsq| estjson[3].Routes |]     :: String
+		let stop4 = [jsq| estjson[3].StopNo |]     :: Integer
+		let name5 = [jsq| estjson[4].Name |] :: String
+		let routes5 = [jsq| estjson[4].Routes |]     :: String
+		let stop5 = [jsq| estjson[4].StopNo |]     :: Integer
+		let name6 = [jsq| estjson[5].Name |] :: String
+		let routes6 = [jsq| estjson[5].Routes |]     :: String
+		let stop6 = [jsq| estjson[5].StopNo |]     :: Integer
+		let name7 = [jsq| estjson[6].Name |] :: String
+		let routes7 = [jsq| estjson[6].Routes |]     :: String
+		let stop7 = [jsq| estjson[6].StopNo |]     :: Integer
+		let name8 = [jsq| estjson[7].Name |] :: String
+		let routes8 = [jsq| estjson[7].Routes |]     :: String
+		let stop8 = [jsq| estjson[7].StopNo |]     :: Integer
+		let name9 = [jsq| estjson[8].Name |] :: String
+		let routes9 = [jsq| estjson[8].Routes |]     :: String
+		let stop9 = [jsq| estjson[8].StopNo |]     :: Integer
+		let name10 = [jsq| estjson[9].Name |] :: String
+		let routes10 = [jsq| estjson[9].Routes |]     :: String
+		let stop10 = [jsq| estjson[9].StopNo |]     :: Integer
+		if not (name1 == "null") then do
+			putStrLn name1
+			putStrLn routes1
+			print stop1
+			if not (name2 == "null") then do
+				putStrLn name2
+				putStrLn routes2
+				print stop2
+				if not (name3 == "null") then do
+					putStrLn name3
+					putStrLn routes3
+					print stop3
+					if not (name4 == "null") then do
+						putStrLn name4
+						putStrLn routes4
+						print stop4
+						if not (name5 == "null") then do
+							putStrLn name5
+							putStrLn routes5
+							print stop5
+							if not (name6 == "null") then do
+								putStrLn name6
+								putStrLn routes6
+								print stop6
+								if not (name7 == "null") then do
+									putStrLn name7
+									putStrLn routes7
+									print stop7
+									if not (name8 == "null") then do
+										putStrLn name8
+										putStrLn routes8
+										print stop8
+										if not (name9 == "null") then do
+											putStrLn name9
+											putStrLn routes9
+											print stop9
+											if not (name10 == "null") then do
+												putStrLn name10
+												putStrLn routes10
+												print stop10
+												else putStrLn "Done"
+											else putStrLn "Done"
+										else putStrLn "Done"
+									else putStrLn "Done"
+								else putStrLn "Done"
+							else putStrLn "Done"
+						else putStrLn "Done"
+					else putStrLn "Done"
+				else putStrLn "Done"
+			else putStrLn "Done"
+		else putStrLn msg
